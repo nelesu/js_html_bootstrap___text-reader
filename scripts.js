53a7c412302a8162e6a8b7444502c7fa76fd608e
Nelesu329 <nelesu329@gmail.com>
2020-09-20 14:12:09 +0300

    Refactoring: cleaning

--- scripts.js
+++ scripts.js
@@ -15,29 +15,13 @@ let voices = [];
 const populateVoiceList = () => {
   voices = speechSynthesis.getVoices();
 
-  // const selectedIndex = voicesList.selectedIndex < 0 ? 0 : voicesList.selectedIndex;
   voicesSelect.innerHTML = voices
     .map((voice, index) => voice.lang === LANG_RU || voice.lang === LANG_US || voice.lang === LANG_GB ? `<option value=${index} data-lang="${voice.lang}" data-name="${voice.name}">${voice.name} (${voice.lang})</option>` : null)
     .join("");
-
-
-
-  // if (voicesList[i].default) {
-  //   option.textContent += ' -- DEFAULT';
-  // }
-
-  // option.setAttribute('data-lang', voicesList[i].lang);
-  // option.setAttribute('data-name', voicesList[i].name);
-  // voicesList.appendChild(voicesList);
-
-  // voicesList.selectedIndex = selectedIndex;
 }
 
-
 populateVoiceList();
-// if (speechSynthesis.onvoiceschanged !== undefined) {
-//   speechSynthesis.onvoiceschanged = populateVoiceList;
-// }
+
 
 const say = () => {
   if (speechSynthesis.speaking) {
@@ -53,13 +37,6 @@ const say = () => {
     ssUtterance.pitch = pitch.value;
     ssUtterance.rate = rate.value;
     ssUtterance.voice = voices[voicesSelect.value];
-    console.log(ssUtterance)
-    // let selectedOption = voicesSelect.selectedOptions[0].getAttribute("data-name");
-    // voices.forEach((voice) => {
-    //   if (voice.name === selectedOption) {
-
-    //   }
-    // })
 
     speechSynthesis.speak(ssUtterance)
   }
@@ -69,5 +46,13 @@ const say = () => {
 
 
 document.querySelector("#btn-start").addEventListener('click', say);
-document.querySelector("#btn-stop").addEventListener('click', speechSynthesis.cancel);
-speechSynthesis.addEventListener("voiceschanged", populateVoiceList)
+document.querySelector("#btn-stop").addEventListener('click', () => speechSynthesis.cancel());
+
+rate.addEventListener('input', () => document.querySelector('.rate-value').innerHTML = rate.value);
+pitch.addEventListener('input', () => document.querySelector('.pitch-value').innerText = pitch.value);
+
+voicesSelect.addEventListener('change', () => {
+  say();
+});
+
+speechSynthesis.addEventListener("voiceschanged", populateVoiceList);
